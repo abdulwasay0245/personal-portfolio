f9bd4497247ce79645944862b54dbfaea7d5d771
Abdul Wasay <106981654+abdulwasay0245@users.noreply.github.com>
2024-11-14 18:10:48 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@
 import Header from "@/component/header";
 import Hero from "@/component/hero";
 import Footer from "@/component/footer";
-import Project from "@/component/Project";
-import About from "@/component/about";
-import styles from "@/app/page.module.css"
+
 
 
 export default function Home() {
